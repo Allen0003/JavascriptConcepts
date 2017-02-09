041890279bc3408ca052826f7ff5044b84a3a29e
Allen0003 <apss1943@gmail.com>
2017-02-09 11:52:10 +0800

    Update promise then.js

--- promise then.js
+++ promise then.js
@@ -2,16 +2,12 @@
 
 '同步'執行函式的結果要不就是'回傳一個值'，要不然就是執行到一半發生'例外'，中斷目前程式然後拋出例外。
 
-
 '異步'執行函式的結果要不就是帶有'回傳值的成功'，要不就是帶有'回傳理由的失敗'。
-
 
 promise 物件最後的結果要不然就用一個回傳值來fulfilled(實現)，
 要不然就用一個理由(錯誤)來rejected(拒絕)。
 
-
 Promise的實作中，會確保Promise物件一實體化後就會固定住狀態，要不就是"已實現"，要不就是"已拒絕"
-
 
 const promise = new Promise(function(resolve, reject) {
   // success
@@ -25,7 +21,6 @@ promise.then(function(value) {
 }, function(reason) {
   // on rejection
 })
-
 
 
 *** then ***
